401228b9c9743aef519402af7098298ecd49e1b9
vitoria174 <72430735+vitoria174@users.noreply.github.com>
2024-05-12 15:19:30 -0400

    Add files via upload

---
+++ 2 . Adicione dois números.py
@@ -0,0 +1,3 @@
+#Você recebe duas listas vinculadas não vazias que representam dois números inteiros não negativos. Os dígitos são armazenados em ordem inversa e cada um de seus nós contém um único dígito. Adicione os dois números e retorne a soma como uma lista vinculada. Você pode assumir que os dois números não contêm nenhum zero à esquerda, exceto o próprio número 0.  
+
+ 
